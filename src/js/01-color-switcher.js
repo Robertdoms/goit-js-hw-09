61fd3e0cd4c485f7e3d74dc45ec7d29ba7cf55ce
RobertDoms <robertdoms90@gmail.com>
2022-10-29 23:56:54 +0300

    Update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,8 @@
-
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
