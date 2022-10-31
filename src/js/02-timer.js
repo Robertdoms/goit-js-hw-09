752d8e18373a500456d217dbd3d47fb2665eee89
RobertDoms <robertdoms90@gmail.com>
2022-10-31 22:52:09 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -16,3 +16,25 @@ const refs = {
 const currentTime = new Date();
 
 refs.timerStartBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= currentTime) {
+      Notify.warning('Please choose a date in the  future');
+    } else {
+      refs.timerStartBtn.disabled = false;
+    }
+  },
+};
+refs.timerStartBtn.addEventListener('click', onStartBtnClick);
+flatpickr(refs.timerInput, options);
+
+let timerId = null;
+function onStartBtnClick() {
+  const inputDate = new Date(refs.timerInput.value);
+  refs.timerStartBtn.disabled = true;
+}
