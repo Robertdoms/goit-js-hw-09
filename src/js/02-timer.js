f75aec03717fae4ccdf9f50ff643416b7ab9b4a9
RobertDoms <robertdoms90@gmail.com>
2022-10-31 10:24:07 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,18 @@
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 
-import
+import 'flatpickr/dist/flatpickr.min.css';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  timerInput: document.querySelector('input#datetime-picker'),
+  timerStartBtn: document.querySelector('button[data-start]'),
+  timerDays: document.querySelector('.value[data-days]'),
+  timerHours: document.querySelector('.value[data-hours]'),
+  timerMinutes: document.querySelector('.value[data-minutes]'),
+  timerSeconds: document.querySelector('.value[data-seconds]'),
+};
+
+const currentTime = new Date();
+
+refs.timerStartBtn.disabled = true;
